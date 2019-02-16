a19e978d79b44b18c24ce46402b31d75d2c50caf
Mark Blue <mblue@students.mchenry.edu>
2019-02-16 14:29:06 -0600

    add CalculateCosts() need to remove top banner need to tweak layout

--- app/src/main/java/com/example/mccwireless/MainActivity.java
+++ app/src/main/java/com/example/mccwireless/MainActivity.java
@@ -11,6 +11,11 @@ import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
+    boolean applyDiscount = false;
+    double price1 = 29.99,
+        price2 = 39.99,
+        price3 = 19.99;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,7 +25,6 @@ public class MainActivity extends AppCompatActivity {
                 rbOffer2 = findViewById(R.id.rbOffer2),
                 rbOffer3 = findViewById(R.id.rbOffer3);
 
-        //TextView heysome = findViewById(R.id.txtResult);
 
         RadioGroup rg1 = findViewById(R.id.rg1),
                 rg2 = findViewById(R.id.rg2),
@@ -29,32 +33,21 @@ public class MainActivity extends AppCompatActivity {
         rg1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
-                if(rbOffer1.isChecked()){
-                    //DoSomething();
-                    CalculateCosts(rbOffer1);
-                }
-                else{
-                    //DoSomethingElse();
-                    ApplyOffer(rbOffer1);
-                }
+                CalculateCosts(rbOffer1, rbOffer2, rbOffer3);
             }
         });
 
         rg2.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
-                if(rbOffer2.isChecked()){
-                    //DoSomething();
-                }
+                CalculateCosts(rbOffer1, rbOffer2, rbOffer3);
             }
         });
 
         rg3.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
-                if(rbOffer3.isChecked()){
-                    //DoSomething();
-                }
+                CalculateCosts(rbOffer1, rbOffer2, rbOffer3);
             }
         });
 
@@ -62,10 +55,24 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-    void CalculateCosts(RadioButton rb){
-        TextView heysome = findViewById(R.id.txtResult);
-        heysome.setText("Hello World");
-        Toast.makeText(MainActivity.this, rb.getText().toString() + " was selected",Toast.LENGTH_LONG).show();
+    void CalculateCosts(RadioButton rb1, RadioButton rb2, RadioButton rb3){
+        double totalCost = 0;
+        String totalCostString;
+        TextView txtResult = findViewById(R.id.txtResult);
+        if(rb1.isChecked()){
+            //Add the cost of rb to total
+            totalCost += price1;
+            //check if the discount applies
+            // print total
+        }
+        if(rb2.isChecked()){
+            totalCost += price2;
+        }
+        if(rb3.isChecked()){
+            totalCost += price3;
+        }
+        totalCostString = Double.toString(totalCost);
+        txtResult.setText(totalCostString);
     }
 
     double ApplyOffer(RadioButton rb){
